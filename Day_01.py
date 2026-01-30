2bbc9f21e3dc62c4f5e02410c362de56bddbd69b
Saim Kazmi <saimkazmi017@gmail.com>
2026-01-30 10:20:22 +0530

    updated day 1 code

--- Day_01.py
+++ Day_01.py
@@ -1,1 +1,32 @@
-print("Hello World")
+#Installed Python in my PC
+#print("Hello World")
+#When you run the file hello_world.py, the ending .py indicates that the
+# file is a Python program. Your editor then runs the file through the
+# Python interpreter, which reads through the program and determines
+# what each word in the program means.
+
+#Syntax Highlighting
+# As we write programs, code editor highlights different parts of your program in different ways
+
+#Variables
+# message = "Hello World"
+#We’ve added a variable named message
+# print(message)
+# A variable is a name that refers to a value.
+# The value is the actual data stored and associated with that variable.
+# In our case value is "Hello World"
+#Adding a variable makes a little more work for the Python
+# interpreter. When it processes the first line, it associates the variable
+# message with the "Hello Python world!" text. When it reaches the second
+# line, it prints the value associated with message to the screen.
+# message = "This is my first program"
+# print(message)
+# You can change the value of a variable in your program at any time,
+# and Python will always keep track of its current value.
+
+# Naming a Variable
+# 1. Variable names can only contain letters numbers and underscores and they  cannot start with a number
+# 2. Spaces are not allowed in variable names
+# 3. Avoid using Python keywords and function names as variable names
+# 4. Variable name should be short but descriptive
+
