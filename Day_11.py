05d1cfbc35cfa65216845ade44e8f6e4657ca1ea
Saim Kazmi <saimkazmi017@gmail.com>
2026-03-02 03:39:32 +0530

    Learned working with classes and Instances

--- Day_11.py
+++ Day_11.py
@@ -48,3 +48,89 @@ your_dog.roll_over()
 # instance a unique variable name or it occupies a unique spot in a list or
 # dictionary
 
+# WORKING WITH CLASSES AND INSTANCES
+# You can use classes to represent many real-world situations. Once you
+# write a class, you’ll spend most of your time working with instances
+# created from that class
+
+
+# THE CAR CLASS
+# Let’s write a new class representing a car. Our class will store information about the kind of car we’re working with, and it will have a
+# method that summarizes this information
+class Car:
+    def __init__(self,make,model,year):
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0
+    def get_descriptive_name(self):
+        long_name = f"{self.make} {self.model} {self.year}"
+        return long_name.title()
+    def read_odometer(self):
+        print(f"This car has {self.odometer_reading} miles on it")
+
+    def update_odometer(self, mileage):
+        self.odometer_reading = mileage
+
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+
+
+my_new_car = Car('audi','a4',2024)
+print(my_new_car.get_descriptive_name())
+my_second_car = Car('BMW','M5',2024)
+print(my_second_car.get_descriptive_name())
+
+
+# To make the class more interesting, let’s add an attribute that
+# changes over time. We’ll add an attribute that stores the car’s overall
+# mileage.
+
+# SETTING A DEFAULT VALUE FOR AN ATTRIBUTE
+# When an instance is created, attributes can be defined without being
+# passed in as parameters. These attributes can be defined in the __init__()
+# method, where they are assigned a default value.
+# Adding another attribute
+my_second_car.read_odometer()
+
+# MODIFYING ATTRIBUTE VALUES
+# You can change an attribute’s value in three ways: you can change the
+# value directly through an instance, set the value through a method, or
+# increment the value (add a certain amount to it) through a method.
+
+# MODIFYING AN ATTRIBUTE'S VALUE DIRECTLY
+# The simplest way to modify the value of an attribute is to access the
+# attribute directly through an instance
+my_new_car.odometer_reading = 23
+my_new_car.get_descriptive_name()
+my_new_car.read_odometer()
+
+
+# MODIFYING AN ATTRIBUTE'S VALUE THROUGH A MODEL
+# It can be helpful to have methods that update certain attributes for you.
+# Instead of accessing the attribute directly, you pass the new value to a
+# method that handles the updating internally
+my_new_car.update_odometer(90)
+my_new_car.read_odometer()
+
+# INCREMENTING AN ATTRIBUTE'S VALUE THROUGH A METHOD
+# Sometimes you’ll want to increment an attribute’s value by a certain
+# amount rather than set an entirely new value
+
+my_used_car = Car('subaru', 'outback', 2015)
+print(my_used_car.get_descriptive_name())
+
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
+
+
+
+
+
+
+
+
+
